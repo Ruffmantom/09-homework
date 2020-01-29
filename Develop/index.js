7758aa4d1aadb89096b6e9cde37987308db9749c
Tom Ruff <ruffmantom@gmail.com>
2020-01-28 18:22:54 -0700

    start on homework

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,74 @@
-const questions = [
+var inquirer = require("inquirer");
+var htmlPage = require("./generateHTML");
+const fs = require("fs");
 
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: "location",
+        message: "Where are you from?"
+    },
+    {
+        type: "input",
+        name: "hobby",
+        message: "What is your favorite hobby?"
+    },
+    {
+        type: "input",
+        name: "food",
+        message: "What is your favorite food?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username"
+    },
+    {
+        type: "input",
+        name: "linkedin",
+        message: "Enter your LinkedIn URL."
+    }
 
 ];
 
-function writeToFile(fileName, data) {
+function promtUser() {
+    return inquirer.prompt(questions);
+
 
 }
+promtUser()
+    .then(function (data) {
+        generateHTML(data)
+        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 
-function init() {
+        axios.get(queryUrl).then(function (res) {
+            const repoNames = res.data.map(function (repo) {
+                return repo.name;
+            });
 
-}
-init();
+            const repoNamesStr = repoNames.join("\n");
+
+            fs.writeFile("repos.txt", repoNamesStr, function (err) {
+                if (err) {
+                    throw err;
+                }
+
+                console.log(`Saved ${repoNames.length} repos`);
+            })
+
+            function writeToFile(fileName, data) {
+
+            }
+
+            function init() {
+
+            }
+            init();
+
+
+// from example
